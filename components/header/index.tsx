e04c3d27c429cba904a50eafef76a8fa5bc9dd21
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2024-10-23 12:43:59 -0500

    header changes for locations drop-down
    added optional locations parameter to header to allow for conditionally rendering the select-locations drop-down

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -3,13 +3,16 @@
 import Link from 'next/link'
 import { NavigationType } from '@/types/documents/navigation-type'
 import { GlobalType } from '@/types/documents/global-type'
+import { LocationType } from '@/types/documents/location-type'
+import LocationMenuSelect from '@/components/select-location/index'
 
 interface HeaderProps {
 	navigation: NavigationType[]
+	locations?: LocationType[]
 	global: GlobalType[]
 }
 
-const Header: React.FC<HeaderProps> = ({ navigation, global }) => {
+const Header: React.FC<HeaderProps> = ({ navigation, global, locations }) => {
 	return (
 		<header className='flex items-center justify-between p-4'>
 			<Link href={'/'}>
@@ -18,6 +21,7 @@ const Header: React.FC<HeaderProps> = ({ navigation, global }) => {
 				</h1>
 			</Link>
 			<ul className='flex space-x-4'>
+				{locations && <LocationMenuSelect locations={locations} />}
 				{navigation[0]?.navItems.map(item => (
 					<li key={item.title} className='hover:text-red-500'>
 						<Link href={`/${item.page.slug.current}`}>{item.title}</Link>
